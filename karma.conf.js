2ec06afa4be7c706901479386b815d479996112a
r2t64 <1741713329@qq.com>
2019-05-20 11:52:53 +0800

    ty2

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,22 @@
 // Generated on Mon May 20 2019 11:20:41 GMT+0800 (GMT+08:00)
 
 module.exports = function(config) {
+  var configuration = {
+    // other things
+ 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+  };
+   
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+   
+  config.set(configuration);
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
